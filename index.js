0479ab75d583909a525a71a2ab95ada686a5c134
Lorraine Makuyana <58143262+lorrainemakuyana@users.noreply.github.com>
2021-05-26 10:50:54 +0000

    Pushing all initial code

---
+++ index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// const React = require('react'); 
+// const ReactDOM = require('react-dom'); 
+
+// ReactDOM.render(what to render, where to render it)
+
+ReactDOM.render(
+    <div>
+        <h1>Hello World!</h1>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    </div>,
+  document.getElementById("root")); 
